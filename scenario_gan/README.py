4880f4bb9edb81bded0fdaf4e4c9c666598412ce
Ziyuan Song <39181208+songziyuanszy@users.noreply.github.com>
2025-05-25 16:40:16 +0800

    README.py

--- scenario_gan/README.py
+++ scenario_gan/README.py
@@ -3,7 +3,7 @@ Donate my code for free!!!
 
 The author's name is '''Ziyuan Song'''
 
-What the code does:
+What is the code:
 1. The code aims to generates tomorrow's PV probability prediction based on today's 288-point PV output curve.
 2. A very easy model, may not be pretty well. But with all the functions.
 3. The document should be in "D:\scenario_gan"
